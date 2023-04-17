3004a373822eac830626cd23317c76732439f43d
Binit <binitkoirala17@gmail.com>
2023-04-17 14:50:37 +0545

    new app

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+echo "Enter your commit message"
+read message
 
 echo "Building your app"
 
@@ -5,5 +7,9 @@ flutter build apk --no-tree-shake-icons --release --split-per-abi
 
 mkdir -p ./gen/
 
-mv ./build/app/outputs/flutter-apk/app-armeabi-v7a-release.apk ./gen/display-"$message".apk
+mv ./build/app/outputs/flutter-apk/app-armeabi-v7a-release.apk ./gen/TwoPointO.apk
 
+
+git add .
+git commit -m "$message"
+git push origin main
