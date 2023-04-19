87be9dca38d79ac1dc84641c72bcae9a1b8eaedb
Binit <binitkoirala17@gmail.com>
2023-04-19 18:40:23 +0545

    changed build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ flutter build apk --split-per-abi
 
 mkdir -p ./gen/
 
-mv ./build/app/outputs/flutter-apk/app-armeabi-v7a-release.apk ./gen/TwoPointO.apk
+mv ./build/app/outputs/flutter-apk/*.apk ./gen/
 
 
 git add .
